e3bef24151cf8230edff40d138a6d0edd661eb8a
tayutoh <tayutoh@gmail.com>
2021-01-06 15:01:39 +0900

    feat: 残りのコンテンツ作成

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
-var gulp = require("gulp");
-var webserver = require("gulp-webserver");
-var sass = require("gulp-sass");
-var autoprefixer = require("gulp-autoprefixer");
+const gulp = require("gulp");
+const webserver = require("gulp-webserver");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+
+// 画像圧縮
+const imagemin = require("gulp-imagemin");
+const mozjpeg = require("imagemin-mozjpeg");
+const pngquant = require("imagemin-pngquant");
+const changed = require("gulp-changed");
 
 // ローカルサーバ
 gulp.task("webserver", function(){
@@ -25,6 +31,26 @@ gulp.task("sass:compile", function(){
 gulp.task("sass:watch", function(){
   gulp.watch("./docs/sass/*.scss", gulp.task("sass:compile"));
 })
+
+// 画像圧縮
+gulp.task("img", function() {
+  return gulp
+    .src("./docs/oriImage/*.{png,jpg}")
+    .pipe(changed("image"))
+    .pipe(
+      imagemin([
+        pngquant({
+          quality: [.7, .85],
+          speed: 1
+        }),
+        mozjpeg({
+          quality: 85,
+          progressive: true
+        })
+      ])
+    )
+    .pipe(gulp.dest("./docs/image/"));
+});
 
 gulp.task('watch', gulp.task("sass:compile"), function(){
   gulp.watch("./docs/sass/*.scss", gulp.task("sass:compile"));
